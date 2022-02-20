abd65daf501fafc8a57da77e6ef8dc52630ca4fa
Jesús Hernández <jesushernandez@Jesuss-Mac-mini.local>
2022-02-20 08:56:30 -0600

    feature: adding basic layout :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./Styles/Main.scss";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="main__header">
+        <img src="icons/titleHarry.png" className="main__header--img" />
+        <h2 className="main__header--title">Selecciona tu filtro</h2>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
