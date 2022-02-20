921732dbc0f58a9fe0d1f3b38ceed1019f61f92f
esther <esther030998@gmail.com>
2022-02-20 09:25:04 -0600

    feature: adding navbar styles and funcionality :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Navbar from "./Navbar";
 import "./Styles/Main.scss";
 
 const App = () => {
   return (
     <div className="main">
+      <Navbar />
       <div className="main__header">
         <img src="icons/titleHarry.png" className="main__header--img" />
         <h2 className="main__header--title">Selecciona tu filtro</h2>
---
+++ src/Navbar/index.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { favorites } from "../store/favorites";
+import Modal from "../ModalsComponent/Modal";
+import { useDispatch } from "react-redux";
+import "../Styles/Navbar.scss";
+const navbar = () => {
+  const dispatch = useDispatch();
+  const favList = useSelector((state) => state.fav.favorites);
+  const [show, setShow] = useState(false);
+  console.log("favList ", favList);
+
+  const showModal = () => {
+    setShow(!show);
+  };
+
+  const removeFav = (data) => {
+    dispatch(favorites.actions.removeFavoriteCharacters(data));
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navbar__content">
+        <button className="navbar__content__button">
+          <h4 className="navbar__content__button--title">FAVORITOS </h4>{" "}
+          <img
+            src="icons/saveWhite.png"
+            className="navbar__content__button--img"
+          ></img>
+          <div className="navbar__content__button__content">
+            {favList.length ? (
+              <>
+                {favList.map((fav) => (
+                  <>
+                    <div className="navbar__content__button__content__dropdown">
+                      <img
+                        src={fav.image}
+                        className="navbar__content__button__content__dropdown--img"
+                      />
+                      <p className="navbar__content__button__content__dropdown--title">
+                        {fav.name}
+                      </p>
+                      <div
+                        onClick={() => removeFav(fav)}
+                        className="   navbar__content__button__content__dropdown--icon"
+                      >
+                        <img src="icons/trash.png"></img>
+                      </div>
+                    </div>
+                  </>
+                ))}
+              </>
+            ) : (
+              <p className="navbar__content__button__content--title">
+                {" "}
+                Sin favoritos
+              </p>
+            )}
+          </div>
+        </button>
+      </div>
+      <Modal show={show} setShow={setShow}></Modal>
+      <button className="navbar__button" onClick={showModal}>
+        <h4 className="navbar__button--title"> AGREGAR </h4>
+        <img src="icons/user.png" className="navbar__button--img"></img>
+      </button>
+    </div>
+  );
+};
+
+export default navbar;
